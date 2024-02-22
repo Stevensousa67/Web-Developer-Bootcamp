986c53100e17ba77d1b241318709e05d2ba31301
Steven Sousa <silver.steven67@gmail.com>
2024-02-22 08:32:14 -0500

    Added tests to Coding Exercise  50

--- 3. JavaScript/Coding Exercise 50 - Some & Every.js
+++ 3. JavaScript/Coding Exercise 50 - Some & Every.js
@@ -1,3 +1,17 @@
 function allEvens(array) {
     return array.every(number => number % 2 === 0);
 }
+
+function someEvens(array) {
+    return array.some(number => number % 2 === 0);
+}
+
+// Test Every
+console.log(allEvens([2, 4, 6, 8, 10]));
+console.log(allEvens([1, 3, 5, 7, 9]));
+console.log(allEvens([2, 4, 5, 8, 10]));
+
+// Test Some
+console.log(someEvens([2, 4, 6, 8, 10]));
+console.log(someEvens([1, 3, 5, 7, 9]));
+console.log(someEvens([2, 4, 5, 8, 10]));
