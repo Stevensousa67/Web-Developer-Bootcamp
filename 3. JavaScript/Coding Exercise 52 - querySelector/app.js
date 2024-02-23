9cd5885d898aef4d5b53904736967d05dc411ecb
Steven Sousa <silver.steven67@gmail.com>
2024-02-23 07:59:35 -0500

    Updated Coding Exercise 52

--- 3. JavaScript/Coding Exercise 52 - querySelector/app.js
+++ 3. JavaScript/Coding Exercise 52 - querySelector/app.js
@@ -1,3 +1,3 @@
-// Write your code in here:
-let image = document.getElementById('unicorn');
-let heading = document.getElementById('mainheading');
+// Your code goes in here!
+let doneTodos = document.querySelectorAll('.done');
+let checkbox = document.querySelector('#scales');
