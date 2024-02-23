50c9fa2fb968cc3904e8a618cf8ea23d340a0550
Steven Sousa <silver.steven67@gmail.com>
2024-02-23 08:02:49 -0500

    FInished Coding Exercise 54

---
+++ 3. JavaScript/Coding Exercise 54 - Manipulating Attributes/app.js
@@ -0,0 +1,5 @@
+// The url you need: 'https://devsprouthosting.com/images/chicken.jpg'
+
+let image = document.querySelector('img');
+image.setAttribute('src', 'https://devsprouthosting.com/images/chicken.jpg');
+image.setAttribute('alt', 'chicken');
