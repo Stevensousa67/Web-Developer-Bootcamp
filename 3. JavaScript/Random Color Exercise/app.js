31b45ba118c20c24122f393195d680a6114102bf
Steven Sousa <silver.steven67@gmail.com>
2024-02-26 09:41:37 -0500

    refined algorithm

--- 3. JavaScript/Random Color Exercise/app.js
+++ 3. JavaScript/Random Color Exercise/app.js
@@ -2,9 +2,14 @@ let h1 = document.querySelector('.main-title');
 let button = document.querySelector('#click-me');
 
 button.addEventListener('click', () => {
-    let r = Math.floor(Math.random() * 256); // Generate R value for RGB
-    let g = Math.floor(Math.random() * 256); // Generate G value for RGB
-    let b = Math.floor(Math.random() * 256); // Generate B value for RGB
-    h1.innerText = `RGB(${r}, ${g}, ${b})`;
-    document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    let newColor = generateRandomColor();
+    h1.innerText = newColor;
+    document.body.style.backgroundColor = newColor;
 })
+
+const generateRandomColor = () => {
+    let r = Math.floor(Math.random() * 255); // Generate R value for RGB
+    let g = Math.floor(Math.random() * 255); // Generate G value for RGB
+    let b = Math.floor(Math.random() * 255); // Generate B value for RGB
+    return `RGB(${r}, ${g}, ${b})`;
+}
