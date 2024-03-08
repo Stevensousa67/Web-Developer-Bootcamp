d354640590d81aae05270b506559ca5c3a04915c
Steven Sousa <silver.steven67@gmail.com>
2024-03-08 11:43:01 -0500

    Implemented CRU from CRUD

--- 4. Node/RESTDemo/index.js
+++ 4. Node/RESTDemo/index.js
@@ -1,33 +1,40 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { v4: uuid } = require('uuid');
+const methodOverride = require('method-override');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride('_method'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 let comments = [
     {
+        id: uuid(),
         username: 'Todd',
         comment: 'lol that is so funny!'
     },
     {
+        id: uuid(),
         username: 'Skyler',
         comment: 'I like to go birdwatching with my dog.'
     },
     {
+        id: uuid(),
         username: 'Sk8erBoi',
         comment: 'Plz delete ur account, Todd'
     },
     {
+        id: uuid(),
         username: 'onlysayswoof',
         comment: 'woof woof woof'
     }
 ]
 
 app.get('/comments', (req, res) => {
-    res.render('comments/index', {comments})
+    res.render('comments/index', { comments })
 })
 
 app.get('/comments/new', (req, res) => {
@@ -36,8 +43,28 @@ app.get('/comments/new', (req, res) => {
 
 app.post('/comments', (req, res) => {
     const { username, comment } = req.body;
-    comments.push({ username, comment });
-    res.send('It worked!');
+    comments.push({ id: uuid(), username, comment });
+    res.redirect('/comments');
+})
+
+app.get('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/show', { comment });
+})
+
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/edit', { comment });
+})
+
+app.patch('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const newComment = req.body.comment;
+    const oldComment = comments.find(c => c.id === id);
+    oldComment.comment = newComment;
+    res.redirect('/comments');
 })
 
 app.get('/tacos', (req, res) => {
