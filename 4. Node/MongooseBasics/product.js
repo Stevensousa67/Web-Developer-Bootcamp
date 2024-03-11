243ea26ead60ada24376372a29a16680cd823287
Steven Sousa <silver.steven67@gmail.com>
2024-03-11 06:41:43 -0400

    Added instance and static methods

--- 4. Node/MongooseBasics/product.js
+++ 4. Node/MongooseBasics/product.js
@@ -34,15 +34,38 @@ const productSchema = new mongoose.Schema({
     }
 });
 
+productSchema.statics.fireSale = function () {
+    return this.updateMany({}, { onSale: true, price: 0 });
+}
+
+productSchema.methods.toggleOnSale = function () {
+    this.onSale = !this.onSale;
+    return this.save();
+}
+
+productSchema.methods.addCategory = function (newCat) {
+    this.categories.push(newCat);
+    return this.save();
+}
+
+
 const Product = mongoose.model('Product', productSchema);
+
+const findProduct = async () => {
+    
+}
+
+Product.fireSale().then(data => {
+    console.log(data);
+})
 
 // const bike = new Product({ name: 'Tire Pump', price: 19.99, categories: ['Cycling'] });
 
-Product.findOneAndUpdate({ name: 'Tire Pump' }, { price: 39 }, { new: true, runValidators: true })
-    .then(data => {
-        console.log('It worked');
-        console.log(data);
-    }).catch(err => {
-        console.log('Process failed');
-        console.log(err);
-})
+// Product.findOneAndUpdate({ name: 'Tire Pump' }, { price: 39 }, { new: true, runValidators: true })
+//     .then(data => {
+//         console.log('It worked');
+//         console.log(data);
+//     }).catch(err => {
+//         console.log('Process failed');
+//         console.log(err);
+// })
