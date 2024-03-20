fde685ef7d2adca437a4e837b515c8da3937406c
Steven Sousa <silver.steven67@gmail.com>
2024-03-20 07:57:13 -0400

    Finished Clicker exercise

---
+++ 5. React/clicker/src/App.js
@@ -0,0 +1,12 @@
+import Clicker from './Clicker';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Button">
+      <Clicker buttonText="Click me!" message="Hello, world!" />
+    </div>
+  );
+}
+
+export default App;
