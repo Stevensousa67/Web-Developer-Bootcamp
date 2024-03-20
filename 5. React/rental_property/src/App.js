789926ff676a5ddd385ae22a9f50720668e0d83e
Steven Sousa <silver.steven67@gmail.com>
2024-03-20 06:54:49 -0400

    Finished rental property exercise

--- 5. React/rental_property/src/App.js
+++ 5. React/rental_property/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import PropertyList from "./PropertyList";
+import "./App.css";
 
-function App() {
+const properties = [
+  { id: 129031, name: "Desert Yurt", rating: 4.9, price: 150 },
+  { id: 823423, name: "Ocean View", rating: 4.5, price: 200 },
+  { id: 239123, name: "Mountain Cabin", rating: 4.8, price: 175 },
+  { id: 932423, name: "Urban Apartment", rating: 4.7, price: 100 },
+  { id: 239123, name: "Lake House", rating: 4.6, price: 225 },
+  { id: 932423, name: "Suburban Home", rating: 4.7, price: 100 },
+  { id: 239123, name: "Beach Bungalow", rating: 4.6, price: 225 },
+  { id: 932423, name: "City Condo", rating: 4.7, price: 100 },
+  { id: 239123, name: "Cabin", rating: 4.6, price: 225 },
+  { id: 932423, name: "Apartment", rating: 4.7, price: 100 },
+  { id: 239123, name: "House", rating: 4.6, price: 225 },
+  { id: 932423, name: "Condo", rating: 4.7, price: 100 },
+  { id: 239123, name: "Bungalow", rating: 4.6, price: 225 },
+  { id: 932423, name: "Yurt", rating: 4.7, price: 100 },
+  { id: 239123, name: "Cottage", rating: 4.6, price: 225 },
+  { id: 932423, name: "Mansion", rating: 4.7, price: 100 },
+  { id: 239123, name: "Villa", rating: 4.6, price: 225 },
+  { id: 932423, name: "Castle", rating: 4.7, price: 100 },
+  { id: 239123, name: "Palace", rating: 4.6, price: 225 },
+  { id: 932423, name: "Estate", rating: 4.7, price: 100 },
+  { id: 239123, name: "Farmhouse", rating: 4.6, price: 225 },
+  { id: 932423, name: "Ranch", rating: 4.7, price: 100 },
+  { id: 239123, name: "Cabin", rating: 4.6, price: 225 },
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <PropertyList properties={properties} />
     </div>
   );
 }
-
-export default App;
