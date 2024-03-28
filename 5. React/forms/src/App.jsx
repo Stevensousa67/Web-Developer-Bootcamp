3b8e3cd599788eb65f6391d6aae40ef139599ba4
Steven Sousa <silver.steven67@gmail.com>
2024-03-28 08:33:14 -0400

    Started Form section

---
+++ 5. React/forms/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import SignUpForm from './SignUpForm'
+import UsernameForm from './UsernameForm'
+
+export default function App() {
+  return (
+    <>
+      <UsernameForm />
+      <SignUpForm />
+    </>
+  )
+}
