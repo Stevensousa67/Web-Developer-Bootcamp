e0246ce9bd87b71ca8f4472688ee2862c4fef72c
Steven Sousa <silver.steven67@gmail.com>
2024-10-11 17:51:26 -0400

    Started YelpCamp project

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
 
@@ -14,6 +15,8 @@ db.once('open', () => {
 
 const app = express();
 
+app.use(express.static('public'));
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
